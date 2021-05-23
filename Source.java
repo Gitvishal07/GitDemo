1ee75f86dfb03e708796c70cff9575ad4ccff165
Gitvishal07 <vishal.sanjay.pawar01@gmail.com>
2021-05-23 19:48:12 +0530

    Added Source file

---
+++ Source.java
@@ -0,0 +1,12 @@
+public class Source {
+
+     public static void main(String[] args) {
+
+
+	// Initializing variable 
+
+	int firstVariable = 100;
+	
+     System.out.println(firstVariable);
+}
+}
